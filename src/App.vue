b7d1d164fb99f281b5ee86aa66cd70ed000568b5
Zayyad Muhammad Sani <zayyadmuhammed700@gmail.com>
2024-07-06 14:41:57 +0100

    Added Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ const { questions } = useQuestionsStore();
 
 <template>
   <main>
+    <button class="btn btn-warning">Person</button>
     <ul>
       <li v-for="question in questions">
         {{ question }}
@@ -13,32 +14,3 @@ const { questions } = useQuestionsStore();
     </ul>
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-import './assets/main.css'
-
+import '../src/assets/styles.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
