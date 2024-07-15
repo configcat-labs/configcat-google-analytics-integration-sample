0e779cb668c52060262907b024d1f4dcc42135f6
Zayyad Muhammad Sani <zayyadmuhammed700@gmail.com>
2024-07-15 08:52:15 +0100

    Modified splash screen

--- src/App.vue
+++ src/App.vue
@@ -17,9 +17,9 @@ onMounted(() => {
 <template>
     <div class="container d-flex flex-column justify-content-center align-items-center">
       <main>
-        <div v-if="!gameStarted" class="splash-screen">
-        <p>Animal Sounds</p>
-        <button class="btn lemon-green" @click="gameStarted = true">Start</button>
+        <div v-if="!gameStarted" id="splash__screen">
+        <p id="app__title">Animal Sounds</p>
+        <button class="btn lawn-green__button" @click="gameStarted = true">Start</button>
       </div>
       <div v-else>
         <QuizContainer
@@ -33,8 +33,20 @@ onMounted(() => {
 
 <style scoped>
 
-  .lemon-green {
+  #splash__screen {
+    margin-top: 100px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
+
+  #app__title {
+    margin-bottom: 50px;
+    font-size: 1.5rem;
+  }
+
+  .lawn-green__button {
     background-color: lawngreen;
-    color: rgb(253, 250, 245);
+    color: rgb(26, 65, 21);
   }
 </style>
