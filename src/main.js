1c1837dc620c1bcba26debfb9c9cf27332625e08
Zayyad Muhammad Sani <zayyadmuhammed700@gmail.com>
2024-07-26 09:42:59 +0100

    Added configcat-vue to main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import '../src/assets/styles.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { ConfigCatPlugin } from "configcat-vue";
 import App from './App.vue'
 import "bootstrap";
 
 const app = createApp(App)
 
 app.use(createPinia())
+
+app.use(ConfigCatPlugin, {
+    sdkKey: import.meta.env.VITE_CONFIGCAT_SDK_KEY,
+});
 
 app.mount('#app')
